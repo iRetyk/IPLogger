d576ac0f74d8070b08f4d7e582818e258c6df4ae
Idan Retyk <idaniretyk@gmail.com>
2025-03-23 14:15:22 +0200

    organize and made debug mode

--- src/spoof.py
+++ src/spoof.py
@@ -5,7 +5,7 @@ and than kills it, when shutting down.
 """
 
 
-
+DEBUG = True
 
 
 import sys
@@ -20,19 +20,41 @@ if (len(sys.argv) != 3):
     sys.exit(0)
 
 
-host_ip,target_ip,router_ip = sys.argv
+class Spoof():
+    def __init__(self) -> None:
+        self.__spoof_obj : Spoofer = Spoofer(*sys.argv)
+        # Setup signal handler function as the signal function.
+        signal.signal(signal.SIGTERM,self.signal_handler)
+
+    def signal_handler(self,sig,frame):
+        print("Shutting down....")
+        self.__spoof_obj.checkout()
+        sys.exit(0)
 
 
+    def spoof(self):
+        while True:
+            self.__spoof_obj.spoof()
+            time.sleep(random.random()/5) # wait a random number between 0-0.2
+            # The time.sleep has 2 jobs - 
+            #   first, make the spoof harder to notice.
+            #   second, the signal can only be received if the process is waiting, and not running.
+            #       thus, without the time the signal wouldn't be registered.
 
 
-def signal_handler(sig,frame):
-    print("Shutting down....")
-    spoof_obj.checkout()
-    sys.exit(0)
-signal.signal(signal.SIGTERM,signal_handler)
+def debug_main():
+    while True:
+        print("Sent packet.... (not really)")
+        time.sleep(1)
 
-spoof_obj : Spoofer = Spoofer(*sys.argv)
-while True:
-    spoof_obj.spoof()
-    time.sleep(random.random()/5) # wait a random number between 0-0.2
+
+def main():
+    spoofer = Spoof()
+    spoofer.spoof()
+
+if __name__ == "__main__":
+    if DEBUG:
+        debug_main()
+    else:
+        main()
 
