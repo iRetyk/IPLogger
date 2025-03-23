007e2f6fce83cfec0b37c3819de9892b6fdd6ef8
Idan Retyk <idaniretyk@gmail.com>
2025-03-23 13:45:53 +0200

    added doc

--- src/spoof.py
+++ src/spoof.py
@@ -1,5 +1,18 @@
+"""
+this file's purpose is:
+the server should activate this script in a distinct process, 
+and than kills it, when shutting down.
+"""
+
+
+
+
+
 import sys
 import signal
+import time
+import random
+
 from netwroking import Spoofer
 
 if (len(sys.argv) != 3):
@@ -12,13 +25,14 @@ host_ip,target_ip,router_ip = sys.argv
 
 
 
-spoof_obj : Spoofer = Spoofer(*sys.argv)
-spoof_obj.spoof()
-
-
-
 def signal_handler(sig,frame):
     print("Shutting down....")
     spoof_obj.checkout()
     sys.exit(0)
 signal.signal(signal.SIGTERM,signal_handler)
+
+spoof_obj : Spoofer = Spoofer(*sys.argv)
+while True:
+    spoof_obj.spoof()
+    time.sleep(random.random()/5) # wait a random number between 0-0.2
+
