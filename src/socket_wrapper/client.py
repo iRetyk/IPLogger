16190fe114fd1d95bfe830af1656dc7e9950ecbc
Idan Retyk <idaniretyk@gmail.com>
2025-03-30 23:30:20 +0300

    nothing acutaly

--- src/socket_wrapper/client.py
+++ src/socket_wrapper/client.py
@@ -1,6 +1,5 @@
 import socket
 
-
 class Client:
     def __init__(self,ip: str,port: int) -> None:
         self.__ip = ip
@@ -9,4 +8,24 @@ class Client:
         self.__sock = socket.socket()
         
         self.__sock.connect((self.__ip,self.__port))
-        
+    
+    
+    def parse(self, data: bytes):
+        pass
+    
+    def client_hello(self):
+        pass
+    
+    def add_url(self, read_url: str):
+        pass
+    
+    def remove_url(self, url: str):
+        pass
+    
+    def req_info(self, url):
+        """
+        request server for stats about a specific url
+        """
+        pass
+    
+    
--- src/socket_wrapper/server.py
+++ src/socket_wrapper/server.py
@@ -10,4 +10,18 @@ class Server:
         
         self.__sock.bind((self.__ip,self.__port))
         self.__sock.listen(100)
-        
+    
+    def parse(self, data: bytes):
+        pass
+    
+    def server_hello(self):
+        pass
+    
+    def show_stats(self):
+        pass
+    
+    def add_url(self):
+        pass
+    
+    def remove_url(self):
+        pass
