5d4784b606b5f07256a6315cc3a1019cabe1d77e
Idan Retyk <idaniretyk@gmail.com>
2025-04-02 12:03:41 +0300

    setup for gui

--- src/socket_wrapper/client.py
+++ src/socket_wrapper/client.py
@@ -10,22 +10,31 @@ class Client:
         self.__sock.connect((self.__ip,self.__port))
     
     
-    def parse(self, data: bytes):
-        pass
+    def menu(self) -> int:
+        # Input a number from user
+        # According to choice go to matching function.
+        # 1 - add
+        # 2 - get
+        # 3 - remove
+        # 4 - get info
+        raise NotImplementedError
     
     def client_hello(self):
         pass
     
-    def add_url(self, read_url: str):
+    def add_url(self, real_url: str):
+        # Input from user the url.
+        # Than build and return the request.
         pass
     
     def remove_url(self, url: str):
+        # No need for further input.
+        # Build and return the request.
         pass
     
     def req_info(self, url):
-        """
-        request server for stats about a specific url
-        """
+        # Input from user the url.
+        # Than build and return the request.
         pass
     
     
