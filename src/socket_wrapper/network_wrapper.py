a5996b44447f52c804dbfe5b0fb1aa287745a35b
Idan Retyk <idaniretyk@gmail.com>
2025-04-29 23:52:38 +0300

    fixed bug

--- src/socket_wrapper/network_wrapper.py
+++ src/socket_wrapper/network_wrapper.py
@@ -9,16 +9,19 @@ class NetworkWrapper:
         self._serv_sock = socket.socket()
         self._serv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    def recv_by_size(self) -> bytes:
+    def recv_by_size(self,sock = None) -> bytes:
         """Recv message from client, using size field to ensure getting all the message.
 
         Returns:
             bytes: the message from client without size fields.
             b'': if the client disconnects, or other error occurred.
         """
+        if sock is None:
+            sock = self._serv_sock
+        
         msg_size: bytes = b""
         while not b"~" in msg_size:
-            msg_size += self._serv_sock.recv(4)
+            msg_size += sock.recv(4)
             if not msg_size:  # msg_size is empty - Client disconnected
                 return b""  # Return empty string to indicate the issue
 
@@ -26,13 +29,18 @@ class NetworkWrapper:
         size = int(size_in_bytes.decode())
 
         while len(msg) != size:
-            msg += self._serv_sock.recv(128)
+            msg += sock.recv(128)
         
         # log
         print("Received >>>" + msg.decode())
         return msg
-
-    def send_by_size(self, to_send: bytes):
+    
+    
+    def send_by_size(self, to_send: bytes,sock = None):
+        
+        if sock is None:
+            sock = self._serv_sock
+        
         to_send = f'{len(to_send)}~{to_send.decode()}'.encode()
         print(" Sending>>>>> " + to_send.decode())
-        self._serv_sock.send(to_send)
+        sock.send(to_send)
