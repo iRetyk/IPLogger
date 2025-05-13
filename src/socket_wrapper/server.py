e550ebb1d1ab19e2ac3a613d234a81615b34322f
Idan Retyk <idaniretyk@gmail.com>
2025-05-13 13:59:26 +0300

    minor issue

--- src/socket_wrapper/server.py
+++ src/socket_wrapper/server.py
@@ -98,23 +98,6 @@ class Server(NetworkWrapper):
     def retrieve_url(self,urls,fake_url: bytes) -> str:
         return urls.get(fake_url.decode(),"<real_url_here> (debug)")
     
-    def manage_urls(func):
-        @wraps(func)
-        def wrapper(self, *args, **kwargs):
-            try:
-                with open(Server.urls_path, "r") as f:
-                    urls: dict[str, str] = json.load(f)
-            except (json.JSONDecodeError, FileNotFoundError):
-                urls = {}
-            
-            result = func(self, urls, *args, **kwargs)
-            
-            with open(Server.urls_path, "w") as f:
-                json.dump(urls, f, indent=4)
-
-            return result
-        
-        return wrapper
     
     @manage_urls
     def add_url(self, urls, real_url: bytes) -> bytes:
